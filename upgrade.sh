39556c27b94956ef571d97b19a7b58d3b2cdff7e
uck9 <nathan.a.reeves@gmail.com>
2023-04-21 21:30:33 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-VERSION=3.4.7
-PREV_VERSION=3.4.8
-URL=https://github.com/netbox-community/netbox/archive/refs/tags/v$VERSION.tar.gz
+NEW_VERSION=v3.5-beta2
+PREV_VERSION=v3.5-beta2
+URL=https://github.com/netbox-community/netbox/archive/refs/tags/v$NEW_VERSION.tar.gz
 CURDIR=`pwd`
 
-wget https://github.com/netbox-community/netbox/archive/v${VERSION}.tar.gz
-sudo tar -xzf v${VERSION}.tar.gz -C /opt
-sudo ln -sfn /opt/netbox-${VERSION}/ /opt/netbox
+wget https://github.com/netbox-community/netbox/archive/v${NEW_VERSION}.tar.gz
+sudo tar -xzf v${NEW_VERSION}.tar.gz -C /opt
+sudo ln -sfn /opt/netbox-${NEW_VERSION}/ /opt/netbox
 sudo cp /opt/netbox-${PREV_VERSION}/local_requirements.txt /opt/netbox/
 sudo cp /opt/netbox-${PREV_VERSION}/netbox/netbox/configuration.py /opt/netbox/netbox/netbox/
 sudo cp /opt/netbox-${PREV_VERSION}/netbox/netbox/ldap_config.py /opt/netbox/netbox/netbox/
