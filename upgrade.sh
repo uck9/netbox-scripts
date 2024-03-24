7681c8e40a355937615459c4bd3cc782ed5fa9c0
uck9 <nathan.a.reeves@gmail.com>
2024-03-24 12:49:53 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-NEW_VERSION=v3.5-beta2
-PREV_VERSION=3.5-beta2
+NEW_VERSION=3.5-beta2
+CURR_VERSION=3.5-beta2
 CURDIR=`pwd`
 
 wget https://github.com/netbox-community/netbox/archive/v${NEW_VERSION}.tar.gz
 sudo tar -xzf v${NEW_VERSION}.tar.gz -C /opt
 sudo ln -sfn /opt/netbox-${NEW_VERSION}/ /opt/netbox
-sudo cp /opt/netbox-${PREV_VERSION}/local_requirements.txt /opt/netbox/
-sudo cp /opt/netbox-${PREV_VERSION}/netbox/netbox/configuration.py /opt/netbox/netbox/netbox/
-sudo cp /opt/netbox-${PREV_VERSION}/netbox/netbox/ldap_config.py /opt/netbox/netbox/netbox/
-sudo cp -pr /opt/netbox-${PREV_VERSION}/netbox/media/ /opt/netbox/netbox/
-sudo cp -r /opt/netbox-${PREV_VERSION}/netbox/scripts /opt/netbox/netbox/
-sudo cp -r /opt/netbox-${PREV_VERSION}/netbox/reports /opt/netbox/netbox/
-sudo cp /opt/netbox-${PREV_VERSION}/gunicorn.py /opt/netbox/
-sudo cd /opt/netbox
+sudo cp /opt/netbox-${CURR_VERSION}/local_requirements.txt /opt/netbox/
+sudo cp /opt/netbox-${CURR_VERSION}/netbox/netbox/configuration.py /opt/netbox/netbox/netbox/
+sudo cp /opt/netbox-${CURR_VERSION}/netbox/netbox/ldap_config.py /opt/netbox/netbox/netbox/
+sudo cp -pr /opt/netbox-${CURR_VERSION}/netbox/media/ /opt/netbox/netbox/
+sudo cp -r /opt/netbox-${CURR_VERSION}/netbox/scripts /opt/netbox/netbox/
+sudo cp -r /opt/netbox-${CURR_VERSION}/netbox/reports /opt/netbox/netbox/
+sudo cp /opt/netbox-${CURR_VERSION}/gunicorn.py /opt/netbox/
+cd /opt/netbox
 sudo ./upgrade.sh
